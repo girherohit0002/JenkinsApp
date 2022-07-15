aa3fc4bab0ba45075930a128d0a8525347e131fe
girherohit <girherohit0002@gmail.com>
2022-07-15 15:32:52 +0530

    exit command written

--- JenkinsApp/Program.cs
+++ JenkinsApp/Program.cs
@@ -11,13 +11,15 @@ namespace JenkinsApp
             Console.WriteLine("Welcome to Jenkins");
 
             Console.WriteLine("This is changing I am doing in order to whether it is running or not");
-            Console.ReadLine();
 
             Oprations op = new Oprations();
             op.x = 12;
             op.y = 45;
             op.Add();
             op.Multiply();
+
+            Console.WriteLine("Exiting from application...............");
+            Console.ReadLine();
 
         }
     }
